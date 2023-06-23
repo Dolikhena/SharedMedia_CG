c2c5221c7f45e8d5e13cdb9feb0bbcbf7f2db5a5
Collie <57497678+Dolikhena@users.noreply.github.com>
2023-06-23 10:26:28 -0400

    Added ACNH and SBSP effects

---
+++ Soundpaths.lua
@@ -0,0 +1,55 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- 
+LSM:Register("sound", "|cFFFF7C0AACNH_Afraid|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Afraid.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Astonished|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Astonished.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Click|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Click.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Cry|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Cry.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Curiosity|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Curiosity.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Depressed|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Depressed.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Disappointed|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Disappointed.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Distressed|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Distressed.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Embarassed|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Embarassed.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Encouragement|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Encouragement.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Enthusiasm|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Enthusiasm.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Euphoria|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Euphoria.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Furious|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Furious.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Joy|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Joy.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Laugh|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Laugh.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Mistake|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Mistake.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Perplexed|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Perplexed.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Regard|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Regard.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Sigh|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Sigh.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Smug|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Smug.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Sneeze|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Sneeze.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Stupefied|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Stupefied.ogg]])
+LSM:Register("sound", "|cFFFF7C0AACNH_Yawn|r", [[Interface\AddOns\SharedMedia_CG\sound\ACNH_Yawn.ogg]])
+LSM:Register("sound", "|cFFFF7C0ABoo-womp|r", [[Interface\Addons\SharedMedia_CG\sound\boo-womp.ogg]])
+LSM:Register("sound", "|cFFFF7C0ADisappointed|r", [[Interface\Addons\SharedMedia_CG\sound\Disappointed.ogg]])
+LSM:Register("sound", "|cFFFF7C0AFoghorn|r", [[Interface\Addons\SharedMedia_CG\sound\Foghorn.ogg]])
+LSM:Register("sound", "|cFFFF7C0ASqueaky|r", [[Interface\Addons\SharedMedia_CG\sound\squeaky.ogg]])
+LSM:Register("sound", "|cFFFF7C0AMagmorax - Bursting|r", [[Interface\Addons\SharedMedia_CG\sound\Bursting.ogg]])
+LSM:Register("sound", "|cFFFF7C0AMagmorax - Cumsplosion|r", [[Interface\Addons\SharedMedia_CG\sound\Cumsplosion.ogg]])
+LSM:Register("sound", "|cFFFF7C0AMagmorax - Howl|r", [[Interface\Addons\SharedMedia_CG\sound\Howl.ogg]])
+LSM:Register("sound", "|cFFFF7C0AMagmorax - Piddle|r", [[Interface\Addons\SharedMedia_CG\sound\Piddle.ogg]])
+LSM:Register("sound", "|cFFFF7C0AMagmorax - Squart|r", [[Interface\Addons\SharedMedia_CG\sound\Squart.ogg]])
+LSM:Register("sound", "|cFFFF7C0AMagmorax - Tongue|r", [[Interface\Addons\SharedMedia_CG\sound\Tongue.ogg]])
+LSM:Register("sound", "|cFFFF7C0AMagmorax - Toot|r", [[Interface\Addons\SharedMedia_CG\sound\Toot.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
